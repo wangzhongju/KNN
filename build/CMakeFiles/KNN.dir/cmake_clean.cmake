21486a294935ba6f671cd000846ac68c7c16f252
wangzhongju <wangzhongju@goldenridge.ltd>
2019-12-03 16:30:38 +0800

    KNN with background

---
+++ build/CMakeFiles/KNN.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KNN.dir/src/main.cpp.o"
+  "KNN.pdb"
+  "KNN"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KNN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
